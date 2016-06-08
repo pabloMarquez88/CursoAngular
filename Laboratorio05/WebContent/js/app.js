cc0503616b7a45337c3ccb56dcfdd914fa410427
Notebook-i5 <Notebook-i5@Notebook-i5-PC>
2016-06-08 08:40:44 -0300

    Commit Laboratorio 5

---
+++ Laboratorio05/WebContent/js/app.js
@@ -0,0 +1,30 @@
+angular.module("myApp", []).controller("myController",
+		[ '$scope', function($scope) {
+			$scope.resultado = 5 + 4;
+			hacerCalculos($scope);
+
+			$scope.indice = 1;
+			$scope.historial = [ {
+				valor : $scope.mitadValor,
+				indice : $scope.indice
+			} ];
+
+			$scope.aleatorio = function() {
+				$scope.resultado += Math.random();
+				hacerCalculos($scope);
+
+				$scope.historial.push({
+					valor : $scope.mitadValor,
+					indice : $scope.indice
+				});
+				$scope.indice = $scope.indice + 1;
+				console.log($scope.historial);
+
+			};
+
+		} ]);
+
+function hacerCalculos(scope) {
+	scope.entero = Math.floor(scope.resultado);
+	scope.mitadValor = scope.resultado / 2;
+}
